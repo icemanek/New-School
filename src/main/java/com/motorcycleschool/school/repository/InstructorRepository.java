67c1dde0b0a0e7db03efd58c1c2913373849bc44
Emil Sankowski <emil_sankowski@tlen.pl>
2021-08-18 23:48:22 +0200

    test

--- src/main/java/com/motorcycleschool/school/repository/InstructorRepository.java
+++ src/main/java/com/motorcycleschool/school/repository/InstructorRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-@Repository
+//@Repository
 @CrossOrigin(origins = "http://localhost:4200")
 public interface InstructorRepository extends CrudRepository<Instructor, Long> {
 
