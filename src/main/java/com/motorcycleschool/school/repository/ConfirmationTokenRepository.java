70db1ea14b4a8f806de0f98939d33dc539cbbe97
Emil Sankowski <emil_sankowski@tlen.pl>
2021-08-22 15:56:25 +0200

    comm

--- src/main/java/com/motorcycleschool/school/repository/ConfirmationTokenRepository.java
+++ src/main/java/com/motorcycleschool/school/repository/ConfirmationTokenRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 
-//@Service
+@Service
 @Repository
 public interface ConfirmationTokenRepository extends CrudRepository<ConfirmationToken, String> {
 
